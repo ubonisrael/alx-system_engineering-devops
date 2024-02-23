69ec3492ef2a4046c2d29f74181f3ffb2426fccc
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-23 21:49:12 +0100

    added werkzeug to packagws to be installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#This manifest installs a package @ flask
+# This manifest installs a package @ flask
 
-package { 'Flask':
+package { 'flask':
 ensure   => '2.1.0',
+provider => 'pip3'
+}
+
+package { 'werkzeug':
+ensure   => '2.1.1',
 provider => 'pip3'
 }
