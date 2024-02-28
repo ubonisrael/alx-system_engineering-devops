821f36ed5fad4f712907bd98a88d6f3a1efa9ba3
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-28 09:53:14 +0100

    0x0C adv task reconfigured code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -24,40 +24,29 @@ exec { 'update server':
 command => '/usr/bin/sudo /usr/bin/apt-get update'
 }
 
-exec { 'change owner /var/www':
-command => '/usr/bin/sudo /usr/bin/chown -R "$USER":"$USER" /var/www',
-require => Package['nginx']
-}
-
-exec { 'change owner /etc/nginx':
-command => '/usr/bin/sudo /usr/bin/chown -R "$USER":"$USER" /etc/nginx',
-require => Package['nginx']
-}
-
 package { 'nginx':
 ensure  => installed,
 require => Exec['update server']
 }
 
+file { '/etc/nginx/sites-available/default':
+ensure  => file,
+content => $server_config,
+notify => Service['nginx']
+}
+
+service { 'nginx':
+ensure  => running,
+enable  => true,
+require => Package['nginx']
+}
+
 file { '/var/www/html/index.nginx-debian.html':
 ensure  => present,
-content => 'Hello World!',
-require => Exec['change owner /var/www']
+content => 'Hello World!'
 }
 
 file { '/var/www/html/404.html':
 ensure  => present,
-content => "Ceci n'est pas une page",
-require => Exec['change owner /var/www']
-}
-
-file { '/etc/nginx/sites-available/default':
-ensure  => present,
-content => $server_config,
-require => Exec['change owner /etc/nginx']
-}
-
-exec { 'restart nginx':
-command => '/usr/bin/sudo /usr/sbin/service /usr/sbin/nginx restart',
-require => File['/etc/nginx/sites-available/default']
+content => "Ceci n'est pas une page"
 }
