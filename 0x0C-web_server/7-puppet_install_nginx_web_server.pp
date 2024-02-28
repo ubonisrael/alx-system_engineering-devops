2da8d39da6a14af2b52d2dee9e321313cce2c745
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-28 08:59:02 +0100

    0x0C adv task initial attempt

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,63 @@
 # Installs nginx on a server
+$server_config = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location /redirect_me {
+        return 301 https://www.goal.com;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+                root /var/www/html;
+                internal;
+    }
+}
+"
 exec { 'update server':
 command => 'sudo apt-get update'
 }
 
 exec { 'change owner /var/www':
-command => 'sudo chown -R "$USER":"$USER" /var/www'
+command => 'sudo chown -R "$USER":"$USER" /var/www',
+require => Package['nginx']
 }
 
 exec { 'change owner /etc/nginx':
-command => 'sudo chown -R "$USER":"$USER" /etc/nginx'
+command => 'sudo chown -R "$USER":"$USER" /etc/nginx',
+require => Package['nginx']
 }
 
-package { 'install nginx':
+package { 'nginx':
 ensure  => installed,
-name    => nginx,
 require => Exec['update server']
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+ensure  => present,
+content => 'Hello World!',
+require => Exec['change owner /var/www']
+}
+
+file { '/var/www/html/404.html':
+ensure  => present,
+content => "Ceci n'est pas une page",
+require => Exec['change owner /var/www']
+}
+
+file { '/etc/nginx/sites-available/default':
+ensure  => present,
+content => $server_config,
+require => Exec['change owner /etc/nginx']
+}
+
+exec { 'restart nginx':
+command => 'sudo service nginx restart',
+require => File['/etc/nginx/sites-available/default'], File['/var/www/html/404.html'], File['/var/www/html/index.nginx-debian.html']
 }
