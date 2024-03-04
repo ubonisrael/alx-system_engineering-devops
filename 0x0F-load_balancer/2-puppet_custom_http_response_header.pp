7212db313ac3f448810da85b9b5c61abfa203396
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-04 22:03:46 +0100

    0x0F task 2 initial attempt

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,54 @@
+# Installs nginx on a server
+$server_config = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    add_header X-Served-By "{$hostname}";
+
+    location /redirect_me {
+        return 301 https://www.goal.com;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+                root /var/www/html;
+                internal;
+    }
+}
+"
+exec { 'update server':
+command => '/usr/bin/sudo /usr/bin/apt-get update'
+}
+
+package { 'nginx':
+ensure  => installed,
+require => Exec['update server']
+}
+
+file { '/etc/nginx/sites-available/default':
+ensure  => file,
+content => $server_config,
+notify  => Service['nginx']
+}
+
+service { 'nginx':
+ensure  => running,
+enable  => true,
+require => Package['nginx']
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+ensure  => present,
+content => 'Hello World!'
+}
+
+file { '/var/www/html/404.html':
+ensure  => present,
+content => "Ceci n'est pas une page"
+}
