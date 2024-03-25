1bddf554e3b983d7355e4c62b6ba6d5f920e070f
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-25 13:23:02 +0100

    0x15 completed task 0 - made sure code doesnt

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,16 +3,16 @@
 from requests import get
 from sys import argv
 
-user = get("https://jsonplaceholder.typicode.com/users/{}/".
-           format(argv[1])).json()
-tasks = get("https://jsonplaceholder.typicode.com/users/{}/todos".
-            format(argv[1])).json()
-
-no_tasks = len(tasks)
-done_tasks = list(filter(lambda x: x['completed'] is True, tasks))
-no_done_tasks = len(done_tasks)
-
-print("Employee {} is done with tasks({}/{})".format(
-    user['name'], no_done_tasks, no_tasks))
-for task in done_tasks:
-    print("\t {}".format(task['title']))
+if __name__ == "__main__":
+    user = get("https://jsonplaceholder.typicode.com/users/{}/".
+               format(argv[1])).json()
+    tasks = get("https://jsonplaceholder.typicode.com/users/{}/todos".
+                format(argv[1])).json()
+    
+    no_tasks = len(tasks)
+    done_tasks = list(filter(lambda x: x['completed'] is True, tasks))
+    no_done_tasks = len(done_tasks)
+    print("Employee {} is done with tasks({}/{})".format(
+        user['name'], no_done_tasks, no_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task['title']))
