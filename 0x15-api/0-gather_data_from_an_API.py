0022a650d735cfd17ca4367b7ea15a0dffa87198
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-25 13:25:50 +0100

    0x15 completed task 0 - made sure to use get to access values in dict

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
                 format(argv[1])).json()
     
     no_tasks = len(tasks)
-    done_tasks = list(filter(lambda x: x['completed'] is True, tasks))
+    done_tasks = list(filter(lambda x: x.get("completed") is True, tasks))
     no_done_tasks = len(done_tasks)
     print("Employee {} is done with tasks({}/{})".format(
-        user['name'], no_done_tasks, no_tasks))
+        user.get("name"), no_done_tasks, no_tasks))
     for task in done_tasks:
         print("\t {}".format(task['title']))
