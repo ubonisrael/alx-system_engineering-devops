dece8ea8e470c44c32a540616ba9953828713530
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-25 13:28:48 +0100

    0x15 completed task 0 - fixed pycodestyle errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """A script that fetches data with an API and displays it"""
-from sys import argv
 from requests import get
+from sys import argv
 
 if __name__ == "__main__":
     user = get("https://jsonplaceholder.typicode.com/users/{}/".
                format(argv[1])).json()
     tasks = get("https://jsonplaceholder.typicode.com/users/{}/todos".
                 format(argv[1])).json()
-    
+
     no_tasks = len(tasks)
     done_tasks = list(filter(lambda x: x.get("completed") is True, tasks))
     no_done_tasks = len(done_tasks)
-    print("Employee {} is done with tasks({}/{})".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), no_done_tasks, no_tasks))
     for task in done_tasks:
         print("\t {}".format(task['title']))
