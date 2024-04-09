1cc257c5bfbabd2ceae08710e4b0edc4ae7aa491
ubonisrael <akpanudohubonisrael@gmail.com>
2024-04-09 17:15:10 +0100

    0x16 task 0 correction trial3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,13 @@ import requests
 def number_of_subscribers(subreddit):
     """Fetches the number of subscribers on the subreddit"""
     url = "https://reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'ubuntu:0x16.advanced_api/v1'}
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
-        return 0
-    subs = res.json().get('data').get('subscribers')
-    return subs
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/123.0.0.0 Safari/537.36'
+        }
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        subs = res.json().get('data').get('subscribers')
+        return subs
+    return 0
