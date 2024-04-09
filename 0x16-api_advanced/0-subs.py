9b1d47b6ce9f976cd09378321394eab9cd6e12a5
ubonisrael <akpanudohubonisrael@gmail.com>
2024-04-09 17:07:27 +0100

    0x16 task 0 correction 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """A module that contains the 'number_of_subscribers' function"""
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Fetches the number of subscribers on the subreddit"""
     url = "https://reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'ubuntu:0x16.advanced_api/v1'}
-    res = get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
         return 0
     subs = res.json().get('data').get('subscribers')
