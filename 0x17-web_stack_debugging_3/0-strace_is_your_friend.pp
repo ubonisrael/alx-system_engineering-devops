85c83e98dd3a77367c80e138e67ea599bdf04f10
ubonisrael <akpanudohubonisrael@gmail.com>
2024-04-12 20:16:17 +0100

    0x17 fixed bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 
 exec { 'fix bug':
 provider => shell,
-command  => "sed -i 's/phpp/php/g' var/www/html/wp-settings.php",
+command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
 path     => '/usr/local/bin/:/bin/'
 }
